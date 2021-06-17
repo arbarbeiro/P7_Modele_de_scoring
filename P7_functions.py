2784bc6c7f9ad2c3932d67a3db8ccf6c49150224
Rita Barbeiro <pbanarita@gmail.com>
2021-06-17 21:49:51 +0200

    add new files

---
+++ P7_functions.py
@@ -0,0 +1,175 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import sys
+import pandas as pd
+
+
+def convert_types(df, print_info = False):
+    '''Convert data types to reduce memory usage by using 
+    more efficient types for the variables'''
+    
+    original_memory = df.memory_usage().sum()
+    
+    # Iterate through each column
+    for c in df:
+        
+        # Convert ids and booleans to integers
+        if ('SK_ID' in c):
+            df[c] = df[c].fillna(0).astype(np.int32)
+            
+        # Convert objects to category
+        elif (df[c].dtype == 'object') and (df[c].nunique() < df.shape[0]):
+            df[c] = df[c].astype('category')
+        
+        # Booleans mapped to integers
+        elif list(df[c].unique()) == [1, 0]:
+            df[c] = df[c].astype(bool)
+        
+        # Float64 to float32
+        elif df[c].dtype == float:
+            df[c] = df[c].astype(np.float32)
+            
+        # Int64 to int32
+        elif df[c].dtype == int:
+            df[c] = df[c].astype(np.int32)
+        
+    new_memory = df.memory_usage().sum()
+    
+    if print_info:
+        print(f'Original Memory Usage: {round(original_memory / 1e9, 2)} gb.')
+        print(f'New Memory Usage: {round(new_memory / 1e9, 2)} gb.')
+        
+    return df
+
+
+def agg_numeric(df, group_var, df_name):
+    """Aggregates the numeric values in a dataframe. This can
+    be used to create features for each instance of the grouping variable.
+    
+    Parameters
+    --------
+        df (dataframe): 
+            the dataframe to calculate the statistics on
+        group_var (string): 
+            the variable by which to group df
+        df_name (string): 
+            the variable used to rename the columns
+        
+    Return
+    --------
+        agg (dataframe): 
+            a dataframe with the statistics aggregated for 
+            all numeric columns. Each instance of the grouping variable will have 
+            the statistics (mean, min, max, sum; currently supported) calculated. 
+            The columns are also renamed to keep track of features created.
+    
+    """
+    # Remove id variables other than grouping variable
+    for col in df:
+        if col != group_var and 'SK_ID' in col:
+            df = df.drop(columns = col)
+            
+    group_ids = df[group_var]
+    numeric_df = df.select_dtypes('number')
+    numeric_df[group_var] = group_ids
+
+    # Group by the specified variable and calculate the statistics
+    agg = numeric_df.groupby(group_var).agg(['median']).reset_index()
+
+    # Need to create new column names
+    columns = [group_var]
+
+    # Iterate through the variables names
+    for var in agg.columns.levels[0]:
+        # Skip the grouping variable
+        if var != group_var:
+            # Iterate through the stat names
+            for stat in agg.columns.levels[1][:-1]:
+                # Make a new column name for the variable and stat
+                columns.append('%s_%s_%s' % (df_name, var, stat))
+
+    agg.columns = columns
+    return agg
+
+
+def count_categorical(df, group_var, df_name):
+    """Computes counts and normalized counts for each observation
+    of `group_var` of each unique category in every categorical variable
+    
+    Parameters
+    --------
+    df : dataframe 
+        The dataframe to calculate the value counts for.
+        
+    group_var : string
+        The variable by which to group the dataframe. For each unique
+        value of this variable, the final dataframe will have one row
+        
+    df_name : string
+        Variable added to the front of column names to keep track of columns
+
+    
+    Return
+    --------
+    categorical : dataframe
+        A dataframe with counts and normalized counts of each unique category in every categorical variable
+        with one row for every unique value of the `group_var`.
+        
+    """
+    
+    # Select the categorical columns
+    categorical = pd.get_dummies(df.select_dtypes('object'))
+
+    # Make sure to put the identifying id on the column
+    categorical[group_var] = df[group_var]
+
+    # Groupby the group var and calculate the sum and mean
+    categorical = categorical.groupby(group_var).agg(['mean'])
+    
+    column_names = []
+    
+    # Iterate through the columns in level 0
+    for var in categorical.columns.levels[0]:
+        # Iterate through the stats in level 1
+        for stat in ['count_norm']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+    
+    categorical.columns = column_names
+    
+    return categorical
+
+
+def mode_categorical(df, group_var, df_name):
+    """Computes the mode category for each categorical variable 
+    
+    Parameters - same as in count_categorical     
+    """
+    
+    # Select the categorical columns
+    categorical = df.select_dtypes('object')
+
+    # Make sure to put the identifying id on the column
+    categorical[group_var] = df[group_var]
+    
+    cat_agg = {}
+    for col in categorical.columns[:-1]:
+        cat_agg[col] = lambda x: x.mode()[0]
+
+    categorical = categorical.groupby(group_var).agg(cat_agg)
+    
+    #categorical.columns = pd.Index([df_name + e[0] + "_" + e[1]. for e in categorical.columns.tolist()])
+
+    column_names = []
+    # Iterate through the columns in level 0
+    for var in categorical.columns
+        # Iterate through the stats in level 1
+        for stat in ['count_norm']:
+            # Make a new column name
+            column_names.append('%s_%s_%s' % (df_name, var, stat))
+    
+    categorical.columns = column_names
+    
+    
+    return categorical
